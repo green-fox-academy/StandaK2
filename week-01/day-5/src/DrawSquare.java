cac1449015afa126adcdc04af6e6f908aeb9b562
StandaK2 <stanislav.koumar@gmail.com>
2019-04-07 19:54:29 +0200

    Adding src java file with DrawSquare task

---
+++ week-01/day-5/src/DrawSquare.java
@@ -0,0 +1,38 @@
+// Write a program that reads a number from the standard input, then draws a
+// square like this:
+//
+//
+// %%%%%%
+// %    %
+// %    %
+// %    %
+// %    %
+// %%%%%%
+//
+// The square should have as many lines as the number was
+
+import java.util.Scanner;
+
+public class DrawSquare {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter integer number as a square side: ");
+        int side = scanner.nextInt();
+        String sign = "X";
+        String space = " ";
+
+        for (int i = 0; i < side; i++) {
+            for (int j = 0; j < side; j++) {
+                if (i == 0 || i == side - 1) {
+                    System.out.print(sign);
+                } else if (j == 0 || j == side - 1) {
+                    System.out.print(sign);
+                } else if (j > 0 && j < side - 1) {
+                    System.out.print(space);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
