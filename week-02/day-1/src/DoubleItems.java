09255ce8bc84087d87d844954e420e5de67d93c7
Stanislav Koumar <stanislav.koumar@gmail.com>
2019-04-08 15:42:50 +0200

    add code file with other tasks of workshop Functions and Arrays

---
+++ week-02/day-1/src/DoubleItems.java
@@ -0,0 +1,15 @@
+// - Create an array variable named `numList`
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Double all the values in the array
+
+public class DoubleItems {
+    public static void main(String[] args) {
+        int[] numList = {3, 4, 5, 6, 7};
+        int multiplier = 2;
+
+        for (int i = 0; i < numList.length; i++) {
+            numList[i] *= multiplier;
+            System.out.println(numList[i]);
+        }
+    }
+}
