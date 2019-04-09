3860b5978b6c95de5866c5fefdd1ebd8c1149641
Stanislav Koumar <stanislav.koumar@gmail.com>
2019-04-09 22:11:10 +0200

    Adding code files with finished ListIntroduction2 task

--- week-02/day-2/src/ListIntroduction2.java
+++ week-02/day-2/src/ListIntroduction2.java
@@ -1,13 +1,3 @@
-/*
-
-        Remove Durian from List B
-        Add Kiwifruit to List A after the 4th element
-        Compare the size of List A and List B
-        Get the index of Avocado from List A
-        Get the index of Durian from List B
-        Add Passion Fruit and Pummelo to List B in a single statement
-        Print out the 3rd element from List A
-*/
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -31,8 +21,35 @@ public class ListIntroduction2 {
 
         System.out.println(fruitNew);
         System.out.println(fruitNew2);
-        
-        String dorian = "Dorian";
 
+//        Print out whether List A contains Durian or not
+
+        String durian = "Durian";
+        System.out.println(fruit.contains(durian));
+
+//       Remove Durian from List B
+        fruitNew.remove(durian);
+
+//        Add Kiwifruit to List A after the 4th element
+        String kiwi = "Kiwifruit";
+        fruit.add(4, kiwi);
+
+//        Compare the size of List A and List B
+        System.out.println(fruit.equals(fruitNew));
+
+//        Get the index of Avocado from List A
+        String avocado = "Avocado";
+        System.out.println(fruit.indexOf(avocado));
+
+//        Get the index of Durian from List B
+        System.out.println(fruitNew);
+        System.out.println(fruitNew.indexOf(durian));
+
+        //    Add Passion Fruit and Pummelo to List B in a single statement
+        fruitNew.addAll(0, Arrays.asList("Passion Fruit", "Pummelo"));
+        System.out.println(fruitNew);
+
+//        Print out the 3rd element from List A
+        System.out.println(fruit.get(2));
     }
 }
