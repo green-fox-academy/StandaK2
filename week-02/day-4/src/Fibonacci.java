6c61e783d5bdbb25071bcff5a6285bc93dd37ecf
Stanislav Koumar <stanislav.koumar@gmail.com>
2019-04-12 13:17:58 +0200

    Adding code files with other finished Fractal tasks

--- week-02/day-4/src/Fibonacci.java
+++ week-02/day-4/src/Fibonacci.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+//The fibonacci sequence is a famous bit of mathematics, and it happens to have a recursive definition.
+// The first two values in the sequence are 0 and 1 (essentially 2 base cases). Each subsequent value is
+// the sum of the previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21 and so on.
+//
+//Define a recursive fibonacci(n) method that returns the nth fibonacci number, with n=0 representing
+// the start of the sequence.
+
+import java.lang.reflect.Array;
 
 public class Fibonacci {
+    public static void main(String[] args) {
+
+        System.out.println(Fibonacci(0, 5));
+
+    }
+
+    public static int Fibonacci (int number, int counts){
+        if (number < counts){
+            return number + Fibonacci(number + 1, counts;
+        }
+        return 0;
+    }
 }
