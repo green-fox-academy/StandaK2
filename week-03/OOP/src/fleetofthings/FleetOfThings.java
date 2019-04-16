726795c72781d8de2b5be69064d1a424c4fef5ee
Stanislav Koumar <stanislav.koumar@gmail.com>
2019-04-16 16:51:59 +0200

    Adding other code files with OOP tasks

---
+++ week-03/OOP/src/fleetofthings/FleetOfThings.java
@@ -0,0 +1,28 @@
+package fleetofthings;
+
+public class FleetOfThings {
+
+    public static void main(String[] args) {
+        Fleet fleet = new Fleet();
+        // Create a fleet of things to have this output:
+        // 1. [ ] Get milk
+        // 2. [ ] Remove the obstacles
+        // 3. [x] Stand up
+        // 4. [x] Eat lunch
+
+        Thing milk  = new Thing("Get milk");
+        Thing obstacle  = new Thing("Remove the obstacles");
+        Thing standUp  = new Thing("Stand up");
+        Thing eatLunch  = new Thing("Eat lunch");
+
+        fleet.add(milk);
+        fleet.add(obstacle);
+        fleet.add(standUp);
+        fleet.add(eatLunch);
+
+        standUp.complete();
+        eatLunch.complete();
+
+        System.out.println(fleet);
+    }
+}
