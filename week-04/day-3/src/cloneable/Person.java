ae33b90b528f048f9e3dc893fb6d57f2e2d7dfd6
Stanislav Koumar <stanislav.koumar@gmail.com>
2019-04-24 14:30:35 +0200

    Adding code files with Interface tasks

---
+++ week-04/day-3/src/cloneable/Person.java
@@ -0,0 +1,27 @@
+package cloneable;
+
+public class Person {
+
+    String name;
+    int age;
+    String gender;
+
+    public Person(String name, int age, String gender){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public Person(){
+        this("Jane Doe", 30, "female");
+    }
+
+    public void introduce(){
+        System.out.println("Hi, I'm " + this.name + ", a " + this.age + " year old " + this.gender + ".");
+
+    }
+
+    public void getGoal(){
+        System.out.println("My goal is: Live for the moment!");
+    }
+}
---
+++ week-04/day-3/src/cloneable/Student.java
@@ -0,0 +1,35 @@
+package cloneable;
+
+public class Student extends Person implements Cloneable {
+    String previousOrganization;
+    int skippedDays;
+
+    public Student(String name, int age, String gender, String previousOrganization) {
+        super(name, age, gender);
+        this.previousOrganization = previousOrganization;
+        this.skippedDays = 0;
+    }
+
+    public Student() {
+        this("Jane Doe", 30, "female", "The School of Life");
+        this.skippedDays = 0;
+    }
+
+    @Override
+    public Student clone(){
+        return new Student(this.name, this.age, this.gender, this.previousOrganization);
+
+    }
+    public void getGoal() {
+        System.out.println("My goal is: Be a junior software developer.");
+    }
+
+    public void introduce() {
+        System.out.println("Hi, I'm " + this.name + ", a " + this.age + " year old " + this.gender + " from " +
+                previousOrganization + " who skipped " + skippedDays + " days from the course already.");
+    }
+
+    public void skipDays(int numberOfDays){
+        this.skippedDays += numberOfDays;
+    }
+}
